d84d325c63466033bd559a5ca23efd21b3780868
Stefan Smarandache <stefan.smarandache96@gmail.com>
2018-03-03 02:22:01 +0200

    Created:
    -Menu class
    
    Others:
    
    -testing Menu class: working!
    -testing Client class: working!

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@
 public class Main {
 
     public static void main(String args[]){
+        Menu m = new Menu();
+        m.show();
         Client c = new Client("Stefan","Smarandache","adresa1",22,"1960214460055");
         c.getInfo();
     }
---
+++ src/Menu.java
@@ -0,0 +1,6 @@
+
+public class Menu {
+    public void show(){
+        System.out.println("This is a menu...");
+    }
+}
